c19f11545ced048c4e0e98e2ce66ca28b1e2d91e
David Evans <evansdav.a@gmail.com>
2019-10-16 13:49:32 -0700

    Features table now keeps track of individual class levels

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_class_features(char_class, level):
     """Call the 5e API and get features of a class at a designated level"""
     r = requests.get("http://www.dnd5eapi.co/api/classes/"
                      + char_class.lower() + "/level/" + str(level))
-    if r.status_code == 200:
+    if r.status_code == 200 and r.content != b'null':
         features = json.loads(r.content)["features"]
         feature_list = [feature["name"] for feature in features]
     else:
@@ -50,7 +50,7 @@ def index():
 def new_char():
     """Show the form to create a new character"""
     return render_template('char_new.html', char={}, levels=range(1, 21),
-                           classes=get_class_names())
+                           classes=get_class_names(), char_class={})
 
 
 @app.route('/new', methods=['POST'])
@@ -58,9 +58,27 @@ def create_char():
     """Create a new character and insert it into the database"""
     char = {
         "name": request.form.get("name"),
+        "class_levels": {
+            "Barbarian": 0,
+            "Bard": 0,
+            "Cleric": 0,
+            "Druid": 0,
+            "Fighter": 0,
+            "Monk": 0,
+            "Paladin": 0,
+            "Ranger": 0,
+            "Rogue": 0,
+            "Sorcerer": 0,
+            "Warlock": 0,
+            "Wizard": 0,
+            "": 0
+        }
     }
-    for i in range(1, 21):
-        char[str(i)] = request.form.get(str(i))
+    for level in range(1, 21):
+        char_class = request.form.get(str(level))
+        char["class_levels"][char_class] += 1
+        class_level = char["class_levels"][char_class]
+        char[str(level)] = str(char_class) + " " + str(class_level)
 
     char_id = characters.insert_one(char).inserted_id
     return redirect(url_for('show_char', char_id=char_id))
@@ -71,11 +89,17 @@ def show_char(char_id):
     """Display a character"""
     char = characters.find_one({"_id": ObjectId(char_id)})
     char_features = []
+    char_class_list = []
     for level in range(1, 21):
-        char_features.append(get_class_features(char[str(level)],
-                             level))
+        char_class_info = char[str(level)].split()
+        char_class = char_class_info[0]
+        char_class_list.append(char_class)
+        char_class_level = char_class_info[1]
+        feature_list = get_class_features(char_class, char_class_level)
+        char_features.append(feature_list)
     return render_template('char_show.html', char=char, levels=range(1, 21),
-                           classes=get_class_names(), features=char_features)
+                           classes=get_class_names(), features=char_features,
+                           char_class=char_class_list)
 
 
 @app.route('/<char_id>', methods=['POST'])
@@ -83,9 +107,28 @@ def update_char(char_id):
     """Update the information for a character"""
     char = {
         "name": request.form.get("name"),
+        "class_levels": {
+            "Barbarian": 0,
+            "Bard": 0,
+            "Cleric": 0,
+            "Druid": 0,
+            "Fighter": 0,
+            "Monk": 0,
+            "Paladin": 0,
+            "Ranger": 0,
+            "Rogue": 0,
+            "Sorcerer": 0,
+            "Warlock": 0,
+            "Wizard": 0,
+            "": 0
+        }
     }
-    for i in range(1, 21):
-        char[str(i)] = request.form.get(str(i))
+    for level in range(1, 21):
+        char_class = request.form.get(str(level))
+        char["class_levels"][char_class] += 1
+        class_level = char["class_levels"][char_class]
+        char[str(level)] = str(char_class) + " " + str(class_level)
+
     characters.update_one(
         {"_id": ObjectId(char_id)},
         {"$set": char})
